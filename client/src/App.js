c04a8b30de2a5f93da3482b49fb83eddffce2a47
br-to <barabara0224@gmail.com>
2021-07-12 00:30:52 +0900

    firebase認証

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
@@ -8,7 +9,31 @@ import Header from './components/Menu/Header';
 import RegisterComplete from './pages/auth/RegisterComplete';
 import Complete from './pages/auth/Complete';
 
+import { auth } from './firebase';
+import { useDispatch } from 'react-redux';
+
 const App = () => {
+  const dispatch = useDispatch();
+
+  // to check firebase auth state
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const idTokenResult = await user.getIdTokenResult();
+        console.log('user', user);
+        dispatch({
+          type: 'LOGGED_IN_USER',
+          payload: {
+            email: user.email,
+            token: idTokenResult.token,
+          },
+        });
+      }
+    });
+    // cleanup
+    return () => unsubscribe();
+  }, []);
+
   return (
     <>
       <Header />
