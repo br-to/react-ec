36dc991c3c5614154d0d5d3c126172a8c27c06ee
br-to <barabara0224@gmail.com>
2021-07-12 00:31:22 +0900

    メアド、パスワードでログイン

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -1,9 +1,78 @@
-import * as React from "react";
+import React, { useState, useEffect } from 'react';
+import { auth } from '../../firebase';
+import { toast } from 'react-toastify';
+import { useDispatch } from 'react-redux';
 
-const Login = () => (
-  <div>
-    <p>ログイン</p>
-  </div>
-);
+const Login = ({ history }) => {
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // firebaseログイン認証
+      const result = await auth.signInWithEmailAndPassword(email, password);
+      const { user } = result;
+      const idTokenResult = await user.getIdTokenResult();
+
+      dispatch({
+        type: 'LOGGED_IN_USER',
+        payload: {
+          email: user.email,
+          token: idTokenResult.token,
+        },
+      });
+      history.push('/');
+    } catch (error) {
+      console.error(error);
+      toast.error('ログインでエラーが発生しました');
+    }
+  };
+
+  const LoginForm = () => (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <input
+          type="email"
+          className="form-control"
+          value={email}
+          onChange={(e) => setEmail(e.currentTarget.value)}
+          autoFocus
+        />
+      </div>
+      <br />
+      <div className="form-group">
+        <input
+          type="password"
+          className="form-control"
+          value={password}
+          onChange={(e) => setPassword(e.currentTarget.value)}
+          placeholder="パスワード"
+        />
+      </div>
+      <br />
+
+      <button
+        type="submit"
+        className="btn btn-raised"
+        disabled={!email | (password.length < 6)}
+        onClick={handleSubmit}
+      >
+        ログイン
+      </button>
+    </form>
+  );
+  return (
+    <div className="container p-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h4>ログイン</h4>
+          {LoginForm()}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
