9a18001f346891111452ca710af5ccaf60d9f0d9
br-to <barabara0224@gmail.com>
2021-07-12 00:31:04 +0900

    ログアウト

--- client/src/components/Menu/Header.jsx
+++ client/src/components/Menu/Header.jsx
@@ -7,15 +7,28 @@ import {
   SettingOutlined,
 } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
+import firebase from 'firebase';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router';
 
 const { SubMenu, Item } = Menu;
 
 const Header = () => {
   const [current, setCurrent] = useState('home');
+  let dispatch = useDispatch();
+  let history = useHistory();
 
   const handleClick = (e) => {
-    console.log('click ', e);
     setCurrent(e.key);
+  };
+
+  const logout = () => {
+    firebase.auth().signOut();
+    dispatch({
+      type: 'LOGOUT',
+      payload: null,
+    });
+    history.push('/login');
   };
 
   return (
@@ -35,6 +48,9 @@ const Header = () => {
       <SubMenu icon={<SettingOutlined />} title="Username">
         <Item key="setting:1">Option 1</Item>
         <Item key="setting:2">Option 2</Item>
+        <Item key="logout" icon={<UserOutlined />} onClick={logout}>
+          ログアウト
+        </Item>
       </SubMenu>
     </Menu>
   );
