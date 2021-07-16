cc3b9a555d4a3420bf66416e99b58a585d88948f
br-to <barabara0224@gmail.com>
2021-07-17 00:18:26 +0900

    stateでヘッダーのだし分け

--- client/src/components/Menu/Header.jsx
+++ client/src/components/Menu/Header.jsx
@@ -8,7 +8,7 @@ import {
 } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 import firebase from 'firebase';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router';
 
 const { SubMenu, Item } = Menu;
@@ -17,6 +17,7 @@ const Header = () => {
   const [current, setCurrent] = useState('home');
   let dispatch = useDispatch();
   let history = useHistory();
+  let { user } = useSelector((state) => ({ ...state }));
 
   const handleClick = (e) => {
     setCurrent(e.key);
@@ -37,21 +38,30 @@ const Header = () => {
         <Link to="/">Home</Link>
       </Item>
 
-      <Item key="register" icon={<UserAddOutlined />} className="float-right">
-        <Link to="/register">Register</Link>
-      </Item>
-
-      <Item key="login" icon={<UserOutlined />} className="float-right">
-        <Link to="/login">Login</Link>
-      </Item>
-
-      <SubMenu icon={<SettingOutlined />} title="Username">
-        <Item key="setting:1">Option 1</Item>
-        <Item key="setting:2">Option 2</Item>
-        <Item key="logout" icon={<UserOutlined />} onClick={logout}>
-          ログアウト
+      {!user && (
+        <Item key="register" icon={<UserAddOutlined />} className="float-right">
+          <Link to="/register">Register</Link>
         </Item>
-      </SubMenu>
+      )}
+
+      {!user && (
+        <Item key="login" icon={<UserOutlined />} className="float-right">
+          <Link to="/login">Login</Link>
+        </Item>
+      )}
+
+      {user && (
+        <SubMenu
+          icon={<SettingOutlined />}
+          title={user.email && user.email.split('@')[0]}
+        >
+          <Item key="setting:1">Option 1</Item>
+          <Item key="setting:2">Option 2</Item>
+          <Item key="logout" icon={<UserOutlined />} onClick={logout}>
+            ログアウト
+          </Item>
+        </SubMenu>
+      )}
     </Menu>
   );
 };
