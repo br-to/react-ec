b759953d153f257534b2c15780c1dc93e783cada
br-to <barabara0224@gmail.com>
2021-07-17 00:17:49 +0900

    リダイレクト設定

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -1,15 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import { auth } from '../../firebase';
+import { auth, googleAuthProvider } from '../../firebase';
 import { toast } from 'react-toastify';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const Login = ({ history }) => {
   const dispatch = useDispatch();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const { user } = useSelector((state) => ({ ...state }));
+
+  useEffect(() => {
+    if (user && user.token) history.push('/');
+  }, [user, history]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
     try {
       // firebaseログイン認証
       const result = await auth.signInWithEmailAndPassword(email, password);
@@ -25,6 +34,7 @@ const Login = ({ history }) => {
       });
       history.push('/');
     } catch (error) {
+      setLoading(false);
       console.error(error);
       toast.error('ログインでエラーが発生しました');
     }
@@ -63,12 +73,49 @@ const Login = ({ history }) => {
       </button>
     </form>
   );
+
+  const googleLogin = async () => {
+    auth
+      .signInWithPopup(googleAuthProvider)
+      .then(async (result) => {
+        const { user } = result;
+        const idTokenResult = await user.getIdTokenResult();
+        dispatch({
+          type: 'LOGGED_IN_USER',
+          payload: {
+            email: user.email,
+            token: idTokenResult.token,
+          },
+        });
+        history.push('/');
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error(error.message);
+      });
+  };
+
   return (
     <div className="container p-5">
       <div className="row">
         <div className="col-md-6 offset-md-3">
-          <h4>ログイン</h4>
+          {loading ? (
+            <h4 className="text-danger">ローディング中</h4>
+          ) : (
+            <h4>ログイン</h4>
+          )}
           {LoginForm()}
+
+          <button
+            type="submit"
+            className="danger btn btn-raised"
+            onClick={googleLogin}
+          >
+            Google Login
+          </button>
+          <Link to="/forgot/password" className="text-danger float-right">
+            パスワードを忘れた方はこちら
+          </Link>
         </div>
       </div>
     </div>
