08b21f9ae8baec3a6f11200c4cb8500a5bbd512f
br-to <barabara0224@gmail.com>
2021-07-23 18:08:13 +0900

    backend側api追加

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,27 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-
 // middlewares
-const { authCheck } = require('../middlewares/auth');
+const { authCheck, adminCheck } = require('../middlewares/auth');
 
 // controller
-const { createOrUpdate } = require('../controllers/auth');
-
-const myMiddleware = (req, res, next) => {
-  console.log('kobaradesu');
-  next();
-};
+const { createOrUpdateUser, currentUser } = require('../controllers/auth');
 
 // route
-//
-router.post('/create-or-update-user', authCheck, createOrUpdate);
-
-router.get('/testing', myMiddleware, (req, res) => {
-  res.json({
-    data: 'Hello Kobara',
-  });
-});
-
+router.post('/create-or-update-user', authCheck, createOrUpdateUser);
+router.post('/current-user', authCheck, currentUser);
+router.post('/current-admin', authCheck, adminCheck, currentUser);
 
 module.exports = router;
