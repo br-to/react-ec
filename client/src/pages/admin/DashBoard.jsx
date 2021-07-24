9a4d4a53b5568b243d0ada8db94f70db389029e5
br-to <barabara0224@gmail.com>
2021-07-24 13:33:02 +0900

    admin dashboard修正

--- client/src/pages/admin/DashBoard.jsx
+++ client/src/pages/admin/DashBoard.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import AdminNav from '../../components/Menu/AdminNav';
 
-const DashBoard = () => {
-  return (
-    <>
-      <h2>admin dashboard!</h2>
-    </>
-  );
-};
+const DashBoard = () => (
+  <div className="container-fluid">
+    <div className="row">
+      <div className="col-md-2">
+        <AdminNav />
+      </div>
+      <div className="col">admin dashboard</div>
+    </div>
+  </div>
+);
 
 export default DashBoard;
