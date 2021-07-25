fc630d481c6fb828d3515d8535aa2c6b17c8d8f6
br-to <barabara0224@gmail.com>
2021-07-25 10:12:39 +0900

    サブカテゴリのバックエンドapi作成

---
+++ server/controllers/sub.js
@@ -0,0 +1,43 @@
+const Sub = require('../models/sub');
+const slugify = require('slugify');
+
+exports.create = async (req, res) => {
+  try {
+    const { name, parent } = req.body;
+    res.json(await new Sub({ name, parent, slug: slugify(name) }).save());
+  } catch (err) {
+    console.log(err);
+    res.status(400).send('Create sub category failed');
+  }
+};
+
+exports.list = async (req, res) => {
+  res.json(await Sub.find({}).exec());
+};
+
+exports.read = async (req, res) => {
+  res.json(await Sub.findOne({ slug: req.params.slug }).exec());
+};
+
+exports.update = async (req, res) => {
+  const { name } = req.body;
+  try {
+    const updated = await Sub.findOneAndUpdate(
+      { slug: req.params.slug },
+      { name, slug: slugify(name) },
+      { new: true }
+    );
+    res.json(updated);
+  } catch (err) {
+    res.status(400).send('sub category updated failed');
+  }
+}
+
+exports.remove = async (req, res) => {
+  try {
+    const deleted = await Sub.findOneAndDelete({ slug: req.params.slug });
+    res.json(deleted);
+  } catch (err) {
+    res.status(400).send('sub category deleted failed');
+  }
+};
