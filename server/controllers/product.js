171f050e45f3aa35d0e319c8c7c43270d8465ceb
br-to <barabara0224@gmail.com>
2021-08-15 21:31:58 +0900

    backendapi作成

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -15,6 +15,49 @@ exports.create = async (req, res) => {
   }
 };
 
-exports.list = async (req, res) => {
-  res.json(await Product.find({}));
+exports.allList = async (req, res) => {
+  let products = await Product.find({})
+    .limit(parseInt(req.params.count))
+    .populate('category')
+    .populate('subs')
+    .sort([['createdAt', 'desc']])
+    .exec();
+  res.json(products);
 };
+
+exports.read = async (req, res) => {
+  let product = await Product.findOne({ slug: req.params.slug })
+    .populate('category')
+    .populate('subs')
+    .exec();
+  res.json(product);
+}
+
+exports.update = async (req, res) => {
+  try {
+    if (req.body.title) {
+      req.body.slug = slugify(req.body.title);
+    }
+    const updated = await Product.findOneAndUpdate(
+      { slug: req.params.slug },
+      req.body,
+      { new: true }
+    ).exec();
+    res.json(updated);
+  } catch (err) {
+    console.log('product update error ----> ', err);
+    res.status(400).json({
+      err: err.message,
+    });
+  }
+}
+
+exports.remove = async (req, res) => {
+  try {
+    const deleted = await Product.findOneAndDelete({ slug: req.params.slug });
+    res.json(deleted);
+  } catch (err) {
+    res.status(400).send('product deleted failed');
+  }
+};
+
