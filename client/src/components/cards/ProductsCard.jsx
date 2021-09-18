6258f9eafb8131c1ca66bfb7e6d19ad69298d528
br-to <barabara0224@gmail.com>
2021-09-18 13:12:05 +0900

    レビュー画面作成

--- client/src/components/cards/ProductsCard.jsx
+++ client/src/components/cards/ProductsCard.jsx
@@ -3,39 +3,47 @@ import { Card } from 'antd';
 import NoImage from '../../images/noimage.png';
 import { EyeOutlined, ShoppingCartOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
+import { ShowAverage } from '../../functions/Ratings';
 const { Meta } = Card;
 
 const ProductsCard = ({ product }) => {
   const { title, description, images, slug } = product;
   return (
-    <Card
-      cover={
-        <img
-          alt="product card"
-          src={images && images.length ? images[0].url : NoImage}
-          style={{ height: '150px', objectFit: 'cover' }}
-          className="p-1"
+    <>
+      {product && product.ratings && product.ratings.length > 0 ? (
+        ShowAverage(product)
+      ) : (
+        <div className="text-center pt-1 pb-3">レビューがありません</div>
+      )}
+      <Card
+        cover={
+          <img
+            alt="product card"
+            src={images && images.length ? images[0].url : NoImage}
+            style={{ height: '150px', objectFit: 'cover' }}
+            className="p-1"
+          />
+        }
+        actions={[
+          <Link to={`/products/${slug}`}>
+            <EyeOutlined className="text-primary" />
+            <br />
+            商品詳細へ
+          </Link>,
+          <>
+            <ShoppingCartOutlined className="text-primary" />
+            <br />
+            カートに入れる
+          </>,
+        ]}
+        className="m-2"
+      >
+        <Meta
+          title={title}
+          description={`${description && description.substring(0, 40)}...`}
         />
-      }
-      actions={[
-        <Link to={`/products/${slug}`}>
-          <EyeOutlined className="text-primary" />
-          <br />
-          商品詳細へ
-        </Link>,
-        <>
-          <ShoppingCartOutlined className="text-primary" />
-          <br />
-          カートに入れる
-        </>,
-      ]}
-      className="m-2"
-    >
-      <Meta
-        title={title}
-        description={`${description && description.substring(0, 40)}...`}
-      />
-    </Card>
+      </Card>
+    </>
   );
 };
 
--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -1,23 +1,56 @@
 import React, { useState, useEffect } from 'react';
 import API from '../utils/API';
 import SingleProduct from '../components/cards/SingleProduct';
+import LoadingCard from '../components/cards/LoadingCard';
+import RelatedProduct from '../components/cards/RelatedProduct';
+import { useSelector } from 'react-redux';
 
 const Product = ({ match }) => {
   const [product, setProduct] = useState({});
+  const [related, setRelated] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+  const [star, setStar] = useState(0);
   const { slug } = match.params;
+  const { user } = useSelector((state) => ({ ...state }));
 
   useEffect(() => {
     loadProduct();
   }, [slug]);
 
+  useEffect(() => {
+    // 正しくログインしたユーザーが商品にレビューを加えたか確認
+    if (product.ratings && user) {
+      let exisitingRatingObject = product.ratings.find(
+        (ele) => ele.postedBy.toString() === user._id.toString()
+      );
+      exisitingRatingObject && setStar(exisitingRatingObject.star); // 現在のユーザーのレビュー
+    }
+  }, [product.ratings, user]);
+
   const loadProduct = async () => {
-    await API.getProduct(slug).then((res) => setProduct(res.data));
+    await API.getProduct(slug).then(async (res) => {
+      setProduct(res.data);
+      // load related
+      setLoading(true);
+      await API.getRelatedProducts(res.data._id).then((res) => {
+        setRelated(res.data);
+        setLoading(false);
+      });
+    });
+  };
+
+  const starClick = async (newRate, name) => {
+    setStar(newRate);
+    await API.updateRate(name, newRate, user.token).then((res) => {
+      console.log(res.data);
+      loadProduct();
+    });
   };
 
   return (
     <div className="container-fluid">
       <div className="row pt-4">
-        <SingleProduct product={product} />
+        <SingleProduct product={product} starClick={starClick} star={star} />
       </div>
 
       <div className="row">
@@ -25,6 +58,11 @@ const Product = ({ match }) => {
           <hr />
           <h4>関連商品</h4>
           <hr />
+          {related.length && isLoading ? (
+            <LoadingCard count={3} />
+          ) : (
+            <RelatedProduct related={related} />
+          )}
         </div>
       </div>
     </div>
