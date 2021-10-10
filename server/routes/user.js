f399d3431cf6107b702a46e5d8d9c54c8e0df6c7
br-to <barabara0224@gmail.com>
2021-10-11 00:18:39 +0900

    バックエンドAPI作成

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
-// route
-router.get('/user', (req, res) => {
-  res.json({
-    data: 'user!!!',
-  });
-});
+const { authCheck } = require('../middlewares/auth');
+
+const { userCart, getUserCart, removeUserCart, postUserAddress } = require('../controllers/user');
+
+router.post('/user/cart', authCheck, userCart); // save cart
+router.get('/user/cart', authCheck, getUserCart); // get cart
+router.delete('/user/cart', authCheck, removeUserCart); // get cart
+router.post('/user/address', authCheck, postUserAddress); // save address
 
 module.exports = router;
