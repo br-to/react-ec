535395a0431811f76d467a6d67cddfd507fc7d54
br-to <barabara0224@gmail.com>
2021-10-17 18:12:01 +0900

    購入履歴作成

---
+++ client/src/components/cards/PaymentInfo.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const PaymentInfo = ({ order }) => (
+  <div>
+    <p>
+      <span>注文峰号: {order.paymentIntent.id}</span> /{' '}
+      <span>支払い金額: {order.paymentIntent.amount}円</span> /{' '}
+      <span>通貨: {order.paymentIntent.currency.toUpperCase()}</span> /{' '}
+      <span>支払い方法: {order.paymentIntent.payment_method_types[0]}</span> /{' '}
+      <span>支払いステータス: {order.paymentIntent.status}</span> /{' '}
+      <span>
+        注文日:
+        {new Date(order.paymentIntent.created * 1000).toLocaleString('ja-JP')}
+      </span>
+      /{' '}
+      <span className="badge bg-primary text-white">
+        注文ステータス: {order.orderStatus}
+      </span>
+    </p>
+  </div>
+);
+
+export default PaymentInfo;
--- client/src/pages/user/History.jsx
+++ client/src/pages/user/History.jsx
@@ -1,15 +1,89 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import API from '../../utils/API';
+import { useSelector } from 'react-redux';
 import UserNav from '../../components/Menu/UserNav';
+import { Link } from 'react-router-dom';
+import PaymentInfo from '../../components/cards/PaymentInfo';
+import { CheckCircleOutlined, CloseCircleOutlined } from '@ant-design/icons';
 
-const History = () => (
-  <div className="container-fluid">
-    <div className="row">
-      <div className="col-md-2">
-        <UserNav />
+const History = () => {
+  const [orders, setOrders] = useState([]);
+  const { user } = useSelector((state) => ({ ...state }));
+
+  useEffect(async () => {
+    await API.getOrders(user.token).then((res) => {
+      console.log(res.data);
+      setOrders(res.data);
+    });
+  }, []);
+
+  const showOrderInTable = (order) => (
+    <table className="table table-bordered">
+      <thead className="thead-light">
+        <tr>
+          <th scope="col">商品名</th>
+          <th scope="col">価格</th>
+          <th scope="col">ブランド</th>
+          <th scope="col">色</th>
+          <th scope="col">購入数</th>
+          <th scope="col">配送可能か</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {order.products.map((o, i) => (
+          <tr key={i}>
+            <td>
+              <Link to={`/products/${o.product.slug}`}>
+                <b>{o.product.title}</b>
+              </Link>
+            </td>
+            <td>{o.product.price}</td>
+            <td>{o.product.brand}</td>
+            <td>{o.color}</td>
+            <td>{o.count}</td>
+            <td>
+              {o.product.shipping === 'Yes' ? (
+                <CheckCircleOutlined className="text-success" />
+              ) : (
+                <CloseCircleOutlined className="text-danger" />
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+
+  const showEachOrders = () =>
+    orders.map((order, i) => (
+      <div key={i} className="m-5 p-3 card">
+        <p>決済情報</p>
+        <PaymentInfo order={order} />
+        {showOrderInTable(order)}
+        <div className="row">
+          <div className="col">
+            <p>PDF ダウンロード</p>
+          </div>
+        </div>
       </div>
-      <div className="col">user history page</div>
+    ));
+
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-2">
+          <UserNav />
+        </div>
+        <div className="col text-center">
+          <h4 className="pt-2">
+            {orders.length > 0 ? '購入履歴' : '購入履歴がありません'}
+          </h4>
+          {showEachOrders()}
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default History;
